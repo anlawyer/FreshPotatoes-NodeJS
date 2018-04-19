71ed7ec3386d0eb34da36077adbc206321f9cb28
Alison Lawyer <anlawyer@berkeley.edu>
2018-04-18 23:43:13 -0700

    able to query sqlite db and send response to client

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const sqlite = require('sqlite');
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 const request = require('request');
 const express = require('express');
 const app = express();
 
 const {PORT = 3000, NODE_ENV = 'development', DB_PATH = './db/database.db'} = process.env;
 
-// START SERVER
-Promise.resolve()
-  .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
-  .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
+// ROUTE HANDLER
+function getFilmRecommendations (req, res, next) {
+  try {
+    sqlite.all('SELECT * FROM artists LIMIT 10;')
+      .then(function (response) {
+        res.status(200).send(response);
+      });
+  } catch (err) {
+    next(err);
+  }
+}
 
 // ROUTES
 app.get('/films/:id/recommendations', getFilmRecommendations);
 
-// ROUTE HANDLER
-function getFilmRecommendations (req, res) {
-  res.status(500).send('Not Implemented');
-}
+// START SERVER
+Promise.resolve()
+  .then(() => sqlite.open(DB_PATH, { Promise }))
+  .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
+  .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
 
 module.exports = app;
